0b404957f87a227ed3a97ee7f04a527a507780c5
prashraj24 <p.rash.23@hotmail.com>
2021-05-23 14:57:45 +0530

    bug fix (empty tweet)

--- lib/widgets/addTweetBox.dart
+++ lib/widgets/addTweetBox.dart
@@ -22,69 +22,68 @@ class _AddTweetBoxState extends State<AddTweetBox> {
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
 
-    return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(height: width * 0.1),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: width * 0.75,
-                child: Form(
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  key: _formKeyTweetText,
-                  child: TextFormField(
-                    keyboardType: TextInputType.multiline,
-                    key: ValueKey("tweetTextfield"),
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                        errorStyle: TextStyle(fontSize: 10),
-                        hintText: "create tweet",
-                        enabledBorder: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: Colors.white, width: 1))),
-                    controller: _tweetTextNewController,
-                    obscureText: false,
-                    onChanged: (val) {
-                      tweetTextNew = val;
-                      setState(() {});
-                    },
-                    validator: (value) {
-                      // if (value.isEmpty) {
-                      //   return ' ';
-                      // } else
-                      if (value.length > 280) {
-                        return 'Tweet cannot be greater than 280 characters';
-                      } else {
-                        tweetTextNew = value;
-                        return null;
-                      }
-                    },
-                  ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SizedBox(height: width * 0.1),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: width * 0.75,
+              child: Form(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                key: _formKeyTweetText,
+                child: TextFormField(
+                  keyboardType: TextInputType.multiline,
+                  key: ValueKey("tweetTextfield"),
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                      errorStyle: TextStyle(fontSize: 10),
+                      hintText: "create tweet",
+                      enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.white, width: 1))),
+                  controller: _tweetTextNewController,
+                  obscureText: false,
+                  onChanged: (val) {
+                    tweetTextNew = val;
+                    setState(() {});
+                  },
+                  validator: (value) {
+                    // if (value.isEmpty) {
+                    //   return ' ';
+                    // } else
+                    if (value.length > 280) {
+                      return 'Tweet cannot be greater than 280 characters';
+                    } else {
+                      tweetTextNew = value;
+                      return null;
+                    }
+                  },
                 ),
               ),
-            ],
-          ),
-          SizedBox(height: width * 0.03),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Container(
-                margin: EdgeInsets.only(right: width * 0.16),
-                height: 25,
-                child: RaisedButton(
-                  key: ValueKey("createTweetButton"),
-                  color: Colors.white,
-                  elevation: 5,
-                  onPressed: () async {
-                    if (tweetTextNew == '') {
-                      final snackBarEmptyTweet =
-                          SnackBar(content: Text('Tweet cannot be empty'));
-                      ScaffoldMessenger.of(context)
-                          .showSnackBar(snackBarEmptyTweet);
-                    }
+            ),
+          ],
+        ),
+        SizedBox(height: width * 0.03),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              margin: EdgeInsets.only(right: width * 0.16),
+              height: 25,
+              child: RaisedButton(
+                key: ValueKey("createTweetButton"),
+                color: Colors.white,
+                elevation: 5,
+                onPressed: () async {
+                  if (tweetTextNew == '') {
+                    final snackBarEmptyTweet =
+                        SnackBar(content: Text('Tweet cannot be empty'));
+                    ScaffoldMessenger.of(context)
+                        .showSnackBar(snackBarEmptyTweet);
+                  } else {
                     if (_formKeyTweetText.currentState.validate()) {
                       TweetModel tweetModelNew = TweetModel();
 
@@ -107,17 +106,17 @@ class _AddTweetBoxState extends State<AddTweetBox> {
                           SnackBar(content: Text('Tweet Created!'));
                       ScaffoldMessenger.of(context).showSnackBar(snackBar);
                     }
-                  },
-                  child: Text(
-                    'Create Tweet',
-                    style: TextStyle(fontSize: 14, color: Colors.black),
-                  ),
+                  }
+                },
+                child: Text(
+                  'Create Tweet',
+                  style: TextStyle(fontSize: 14, color: Colors.black),
                 ),
               ),
-            ],
-          ),
-        ],
-      ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
